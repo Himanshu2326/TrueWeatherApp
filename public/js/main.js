1a9fe605a7234d82a17dfb870650f1db6c6a4255
Himanshu2326 <himanshuprajapati71305@gmail.com>
2023-08-17 16:28:29 +0530

    weatherapp

---
+++ public/js/main.js
@@ -0,0 +1,56 @@
+
+console.log('Chal Raha Hu mai');
+
+// Weather API Area:-
+
+let cityName = document.getElementById('cityName');
+let city_name = document.getElementById('city_name')
+let submitBtn = document.getElementById('submitBtn');
+let temp = document.getElementById('temp');
+let data_hide = document.querySelector('.middle_layer')
+let temp_status = document.getElementById('temp_status');
+// let InputArea = document.querySelector('.InputArea');
+
+const getWeatherInfo = async (event) => {
+    event.preventDefault();
+    
+    let cityVal = cityName.value;
+    console.log(`Your City is :${cityVal}`)
+    if (cityVal == "") {
+        city_name.innerText = 'Please Enter City Name';
+        data_hide.classList.add('data_hide')
+    }
+    else {
+        try {
+            let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityVal}&units=metric&appid=73c604d1c335623b5f574390a02b4485`
+            let response = await fetch(url);
+            let data = await response.json();
+            let arrData = [data];
+            
+            city_name.innerHTML = `${arrData[0].name},${arrData[0].sys.country}`;
+            temp.innerHTML = arrData[0].main.temp;
+            temp_status.innerHTML = arrData[0].weather[0].main;
+            data_hide.classList.remove('data_hide');
+            cityName.value = "";
+
+        }
+        catch {
+            city_name.innerText = 'Please Enter A Valid City Name';
+            data_hide.classList.add('data_hide')
+        }
+        
+    }
+}
+
+submitBtn.addEventListener('click', getWeatherInfo);
+
+// Day And Day Area:-
+
+let dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+/*Sample Code*/
+let d = new Date();
+console.log(d);
+console.log("Day Name: " + dayNames[d.getDay()])
+day.innerHTML = dayNames[d.getDay()];
+date.innerHTML = d.getDate();
---
+++ src/app.js
@@ -0,0 +1,47 @@
+
+// Dependencies :-
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+const path = require('path');
+const port = process.env.PORT || 1000  // process.env.PORT it is used When we Host To a Real Port
+
+
+// const PubPath = path.join(__dirname, '../public');
+const ParPath = path.join(__dirname, '../partials');
+const public = path.join(__dirname, '../public');
+
+//  Using hbs:-
+app.set('view engine', 'hbs');
+
+// app.use(express.static(PubPath));
+hbs.registerPartials(ParPath)
+app.use(express.static(public))
+
+// Routing :-
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.get('/index', (req, res) => {
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/weather', (req, res) => {
+    res.render('weather')
+})
+
+app.get('*', (req, res) => {
+    res.render('404')
+})
+
+
+// Live The Port:-
+app.listen(port, () => {
+    console.log(`Port :${port} is Live Now`)
+})
