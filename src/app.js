da0ecdaf6ff7da36b77978ad7a32373ddc0d5289
Himanshu2326 <127850749+Himanshu2326@users.noreply.github.com>
2023-08-19 00:28:36 +0530

    "Final Changes"

--- src/app.js
+++ src/app.js
@@ -2,43 +2,14 @@
 // Dependencies :-
 const express = require('express');
 const app = express();
-const hbs = require('hbs');
 const path = require('path');
 const port = process.env.PORT || 1000  // process.env.PORT it is used When we Host To a Real Port
 
-
-// const PubPath = path.join(__dirname, '../public');
-const ParPath = path.join(__dirname, '../partials');
 const public = path.join(__dirname, '../public');
 
-//  Using hbs:-
-app.set('view engine', 'hbs');
 
-// app.use(express.static(PubPath));
-hbs.registerPartials(ParPath)
 app.use(express.static(public))
 
-// Routing :-
-
-app.get('/', (req, res) => {
-    res.render('index.hbs')
-})
-
-app.get('/index.hbs', (req, res) => {
-    res.render('index.hbs')
-})
-
-app.get('/about.hbs', (req, res) => {
-    res.render('about.hbs')
-})
-
-app.get('/weather.hbs', (req, res) => {
-    res.render('weather.hbs')
-})
-
-app.get('*', (req, res) => {
-    res.render('404.hbs')
-})
 
 
 // Live The Port:-
